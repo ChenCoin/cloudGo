7377c8d3fa7dd15947f61f3b05dfde4404c301e6
ChenCoin <chencoin.cn@gmail.com>
2020-02-12 21:34:26 +0800

    add command

---
+++ command/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+	"strconv"
+)
+
+type Item struct {
+	Path    string `json:"path"`
+	Command string `json:"command"`
+}
+
+type Config struct {
+	Initial bool   `json:"initial"`
+	Port    int    `json:"port"`
+	Bash    string `json:"bash"`
+	List    []Item `json:"list"`
+}
+
+var configPath = "./config.json"
+
+func readConfig() (Config, error) {
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return Config{}, err
+	}
+	conf := Config{}
+	err = json.Unmarshal(data, &conf)
+	return conf, err
+}
+
+func main() {
+	conf, err := readConfig()
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	if conf.Initial == false {
+		log.Print("please initial the configure file, and set \"initial\":true")
+		return
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		match := false
+		for i := 0; i < len(conf.List); i++ {
+			item := conf.List[i]
+			if r.RequestURI == item.Path {
+				cmd := exec.Command(conf.Bash, "-c", item.Command)
+				bytes, err := cmd.Output()
+				if err != nil {
+					w.Write([]byte(err.Error()))
+				} else {
+					w.Write(bytes)
+				}
+				match = true
+				break
+			}
+		}
+		if !match {
+			w.WriteHeader(http.StatusNotFound)
+			http.Error(w, "404", http.StatusNotFound)
+		}
+	})
+	log.Printf("server running on port " + strconv.Itoa(conf.Port))
+	err = http.ListenAndServe(":"+strconv.Itoa(conf.Port), nil)
+	if err != nil {
+		log.Printf("error when create server")
+	}
+}
